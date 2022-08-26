787cc63a61e2aa950ed8abe1219d7876741846b6
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-08-26 15:05:21 +0200

    add hit Api feature

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-import data from './modules/data.js';
+import { newGame, getData } from './modules/Data.js';
 import './style.css';
 
+const name = document.getElementById('name');
+const scores = document.getElementById('score');
+const btn = document.getElementById('btn');
+const refresh = document.getElementById('refresh');
 const list = document.getElementById('list-score');
-data.forEach((scores) => {
-  const li = document.createElement('li');
-  const id = data.indexOf(scores);
 
-  if (id % 2 === 0) {
-    li.classList.add('pair-list');
-  } else {
-    li.classList.add('impair-list');
-  }
+refresh.addEventListener('click', () => window.location.reload());
 
-  li.innerHTML = `Name : ${scores.name}`;
+btn.addEventListener('click', (e) => {
+   e.preventDefault();
+  newGame(name, scores);
+});
 
-  list.append(li);
+const results = getData();
+
+results.then((res) => {
+  const array = res.result;
+  array.forEach((arr) => {
+    const li = document.createElement('li');
+    const id = array.indexOf(arr);
+
+    if (id % 2 === 0) {
+      li.classList.add('pair-list');
+    } else {
+      li.classList.add('impair-list');
+    }
+
+    li.innerHTML = `${arr.user} : ${arr.score}`;
+
+    list.append(li);
+  });
 });
--- src/modules/data.js
+++ src/modules/data.js
@@ -1,10 +1,25 @@
-const data = [
-  {
-    name: 100,
-  },
-  {
-    name: 130,
-  },
-];
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const Id = 'Zl4d7IVkemOTTVg2fUdz';
 
-module.exports = data;
+const newGame = async (name, scores) => {
+  await fetch(`${url}games/${Id}/scores/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name.value,
+      score: scores.value,
+    }),
+    headers: {
+      'content-type': 'Application/json; charset=UTF-8',
+    },
+  }).then((Response) => Response.json());
+  name.value = '';
+  scores.value = '';
+};
+
+const getData = async () => {
+  const result$ = await fetch(`${url}games/${Id}/scores/`);
+  const res = await result$.json();
+  return res;
+};
+
+export { newGame, getData };
